4fd661553fa227a30bb740c2178015a6c300c9fd
Ido Oren <idoorenlil@gmail.com>
2020-10-14 11:41:02 +0300

    deploy

---
+++ precache-manifest.628ad1a6def216ee6d0ed6708767d381.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde81165de36f3df83ab",
+    "url": "/css/app.c3c75925.css"
+  },
+  {
+    "revision": "75b85767aecf5aec57a29e8712fd5f90",
+    "url": "/img/loader.gif"
+  },
+  {
+    "revision": "cf25673ac55afbeb7adb1975cc029a22",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "5698b8a2c0d7b5c12f299b24f1c6487d",
+    "url": "/img/slide1.5698b8a2.png"
+  },
+  {
+    "revision": "5698b8a2c0d7b5c12f299b24f1c6487d",
+    "url": "/img/slide1.png"
+  },
+  {
+    "revision": "5a3d21f674b327e80fd8188f02f94d1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cde81165de36f3df83ab",
+    "url": "/js/app.6a985d9b.js"
+  },
+  {
+    "revision": "2940c8c9715a57af58d8",
+    "url": "/js/chunk-vendors.5ea4eebc.js"
+  },
+  {
+    "revision": "025823a6d9f304b127e70f0c2b04b0d5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
